004e4c87828d62705abebfe10c5e3523242e26a6
Yelyzaveta Baranovska <lizabaranovska133@gmail.com>
2025-11-21 15:39:32 +0200

    fix app

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./normalize.css";
-import "./index.css";
+import App from "./components/App/App";
+import "modern-normalize";
+import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
